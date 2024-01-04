6d9dc03c5e18a11b5b85f79ec547e1b7f3e2afec
Esperanza211 <khadijazaabar05@gmail.com>
2024-01-04 04:42:27 -0800

    binary trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,8 +7,8 @@
  * Return: the new node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
-{
-	binary_tree_t *n_node;
+
+{	binary_tree_t *n_node;
 	n_node = malloc(sizeof(binary_tree_t));
 	if (n_node == NULL)
 	{
